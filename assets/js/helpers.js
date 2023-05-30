991aff3c216ae9dd6232eff523456be8957c44b6
SebastienWeb <sebastienweb27@gmail.com>
2023-05-30 10:40:43 +0200

    Barre de recherche filter

--- assets/js/helpers.js
+++ assets/js/helpers.js
@@ -1,3 +1,6 @@
+import { recipes } from "../../data/recipes.js";
+const containerCard = document.querySelector("#containerCard");
+
 export function generateCardHtml(recipe, ingredientsHtml) {
   const { name, description, image, title } = recipe;
 
@@ -12,6 +15,23 @@ export function generateCardHtml(recipe, ingredientsHtml) {
     </div>
   `;
 }
+
+export function generateRecipeCardHtml(recipe) {
+  const { name, description, image, title } = recipe;
+
+  return `
+  <div class="cardElement">
+    <img class="cardImg w-100" src="../assets/images/${image}" alt="${title}" lazy="loading"/>
+    <div class="paddingElement">
+      <h2>${name}</h2>
+      <span class="d-block pb-3 recipe">RECETTE</span>
+      <p>${description}</p>
+      <span class="d-block pb-3 recipe">INGREDIENTS</span>
+    </div>
+  </div>
+  `;
+}
+
 export function generateIngredientHtml(ingredient) {
   if (ingredient.unit === "" || ingredient.unit === undefined) {
     if (ingredient.quantity) {
@@ -37,4 +57,44 @@ export function generateIngredientHtml(ingredient) {
       </div>
     `;
   }
+}
+
+export function filterRecipes(searchTerm) {
+  const filteredRecipes = [];
+  for (const recipe of recipes) {
+    const { name, ingredients } = recipe;
+    const lowerCaseName = name.toLowerCase();
+    if (
+      lowerCaseName.includes(searchTerm) ||
+      hasIngredientMatch(ingredients, searchTerm)
+    ) {
+      filteredRecipes.push(recipe);
+    }
+  }
+  displayRecipes(filteredRecipes);
+}
+
+export function hasIngredientMatch(ingredients, searchTerm) {
+  for (const ingredient of ingredients) {
+    const { ingredient: ingredientName } = ingredient;
+    if (ingredientName.toLowerCase().includes(searchTerm)) {
+      return true;
+    }
+  }
+  return false;
+}
+export function displayRecipes(recipeData) {
+  let recipesHtml = "";
+  containerCard.innerHTML = "";
+
+  if (recipeData.length === 0) {
+    containerCard.innerHTML = `<p>Aucune recette trouvée</p>`;
+    return;
+  }
+
+  for (const recipe of recipeData) {
+    const recipeHtml = generateRecipeCardHtml(recipe);
+    recipesHtml += recipeHtml;
+  }
+  containerCard.innerHTML = recipesHtml;
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,17 @@
 import { recipes } from "../../data/recipes.js";
-import { generateCardHtml, generateIngredientHtml } from "./helpers.js";
+import {
+  filterRecipes,
+  generateCardHtml,
+  generateIngredientHtml,
+} from "./helpers.js";
 const containerCard = document.querySelector("#containerCard");
+const searchValue = document.getElementById("search");
+searchValue.addEventListener("input", () => {
+  const searchTerm = searchValue.value.trim().toLowerCase();
+  if (searchTerm.length >= 3) {
+    filterRecipes(searchTerm);
+  }
+});
 
 for (const recipe of recipes) {
   const card = document.createElement("div");
